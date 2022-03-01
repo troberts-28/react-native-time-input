f95c0b8f51b8987b5bcef95917d693e10e5f2385
Tim Roberts <tim.roberts20@imperial.ac.uk>
2022-03-01 10:54:02 +0000

    actual mask fix

--- src/helpers/timeInput.ts
+++ src/helpers/timeInput.ts
@@ -14,13 +14,13 @@ export const mask = (value: string): string => {
 
   if (totalCharactersInValue === 5) {
     return (
-      value.substr(0, 2) + ':' + value.substr(2, 3) + ':' + value.substr(3)
+      value.substr(0, 1) + ':' + value.substr(2, 2) + ':' + value.substr(3)
     );
   }
 
   if (totalCharactersInValue === 6) {
     return (
-      value.substr(0, 2) + ':' + value.substr(2, 4) + ':' + value.substr(4)
+      value.substr(0, 2) + ':' + value.substr(2, 2) + ':' + value.substr(4)
     );
   }
 
